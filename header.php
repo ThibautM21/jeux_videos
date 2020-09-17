b29d72ee23d91306dad281bd9c57585fbd3e58c2
ThibautM <thibaut.moy@hotmail.fr>
2020-09-17 15:34:54 +0200

    Ajout header/footer et fichiers css

---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<!-- Google Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/navbar-fixed-left.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>JeuxVideos.com</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand navbar-dark bg-dark fixed-left">
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="editor.php">Editor</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="category.php">Category</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="support.php">Support</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="version.php">Version</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="game.php">Game</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+	<nav class="navbar navbar-expand navbar-dark bg-primary fixed-top">
+		<a class="navbar-brand" href="index.php">JeuxVideos.com</a>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<form class="form-inline ml-auto my-2 my-lg-0" method="post" action="logout.php">
+				<button class="btn btn-outline-light my-2 my-sm-0" type="submit">Log in</button>
+			</form>
+		</div>
+	</nav>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+	require 'header.php';
+
+	require 'inc/libraries.php';
+	require 'footer.php';
