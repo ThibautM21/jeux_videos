40f39dc2897fd5f6dbc054a6a0aedd9a8513dadc
Anthony <verrier-anthony@outlook.fr>
2020-09-17 14:49:36 +0200

    création des class

---
+++ classes/editor.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class editor{	
+
+
+	private $id;
+	private $name;
+	private $link;
+	
+
+// Constucteur
+	public function __construct(array $donnees){
+		foreach ($donnees as $key => $value) {
+			$method='set'.ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->$method($value);
+			}			
+		}
+	}
+
+// Getter
+	public function getId(){return $this->id;}
+	public function getName(){return $this->name;}
+	public function getLink(){return $this->link;}
+
+
+// Setter
+	private function setId($id){
+		$this->id=$id;
+	}
+
+	public function setName($name){
+		$this->name=$name;
+	}
+	
+	public function setLink($link){
+		$this->link=$link;
+	}
+
+}
---
+++ classes/game.class.php
@@ -0,0 +1,68 @@
+<?php
+
+class game{	
+
+
+	private $id;
+	private $title;
+	private $description;
+	private $image;
+	private $link;
+	private $pegi;
+	private $category_id;
+	private $editor_id
+
+// Constucteur
+	public function __construct(array $donnees){
+		foreach ($donnees as $key => $value) {
+			$method='set'.ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->$method($value);
+			}			
+		}
+	}
+
+// Getter
+	public function getId(){return $this->id;}
+	public function getTitle(){return $this->title;}
+	public function getDescription(){return $this->description;}
+	public function getImage(){return $this->image;}
+	public function getLink(){return $this->link;}
+	public function getPegi(){return $this->pegi;}
+	public function getCategory_id(){return $this->category_id;}
+	public function getEditor_id(){return $this->editor_id;}
+
+// Setter
+	private function setId($id){
+		$this->id=$id;
+	}
+
+	public function setTitle($title){
+		$this->title=$title;
+	}
+
+	public function setDescription($description){
+		$this->description=$description;
+	}
+
+	public function setImage($image){
+		$this->image=$image;
+	}
+
+	public function setLink($link){
+		$this->link=$link;
+	}
+
+	public function setPegi($pegi){
+		$this->pegi=$pegi;
+	}
+
+	public function setCategory_id($category_id){
+		$this->category_id=$category_id;
+	}
+
+	public function setEditor_id($editor_id){
+		$this->editor_id=$editor_id;
+	}
+
+}
